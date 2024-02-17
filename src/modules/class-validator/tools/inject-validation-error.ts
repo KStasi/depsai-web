159376e170656d97212bde0cf09d011951241d9f
Zhanna Bieliavska <zhanna.bieliavska@nixsolutions.com>
2024-02-17 21:54:23 +0100

    home page wagmi, tsconfig

--- src/modules/class-validator/tools/inject-validation-error.ts
+++ src/modules/class-validator/tools/inject-validation-error.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
 import { ValidationError } from 'class-validator';
 
 const getRealTarget = (error: ValidationError) => {
@@ -14,32 +15,36 @@ const getRealTarget = (error: ValidationError) => {
  * @returns error if target of error has no 'error' property
  */
 export const injectValidationError = (errors: Array<ValidationError>): Array<ValidationError> => {
-  return errors
-    .map(error => {
-      if (!error.children) {
-        throw new Error('error.children is undefined', { cause: error });
-      }
-      if (!error.target) {
-        throw new Error('error.target is undefined', { cause: error });
-      }
+  if (errors) {
+    return errors
+      .map(error => {
+        if (!error.children) {
+          throw new Error('error.children is undefined', { cause: error });
+        }
+        if (!error.target) {
+          throw new Error('error.target is undefined', { cause: error });
+        }
 
-      if (error.children.length > 0) {
-        error.children = injectValidationError(error.children);
+        if (error.children.length > 0) {
+          error.children = injectValidationError(error.children);
 
-        if (error.children.length) {
+          if (error.children.length) {
+            return error;
+          }
+        } else {
+          const realTarget = getRealTarget(error);
+
+          if (Object.hasOwnProperty.call(realTarget, 'error')) {
+            realTarget.error = error;
+
+            return undefined;
+          }
+
           return error;
         }
-      } else {
-        const realTarget = getRealTarget(error);
+      })
+      .filter((error): error is ValidationError => error !== undefined);
+  }
 
-        if (Object.hasOwnProperty.call(realTarget, 'error')) {
-          realTarget.error = error;
-
-          return undefined;
-        }
-
-        return error;
-      }
-    })
-    .filter(Boolean);
+  return [];
 };
