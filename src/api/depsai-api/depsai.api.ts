984d01cc26e9f13aa7629296d08ac3e671747987
KStasi <nastasiakondaurova@gmail.com>
2024-02-18 12:20:14 +0200

    get deployments from api

--- src/api/depsai-api/depsai.api.ts
+++ src/api/depsai-api/depsai.api.ts
@@ -1,10 +1,26 @@
-// const apiHost = 'http://localhost:3000';
-const apiHost = import.meta.env.VITE_API_HOST;
+const apiHost = 'http://localhost:3000';
+// const apiHost = import.meta.env.VITE_API_HOST;
 
+export type DeploymentDetails = {
+  package: string;
+  command: string;
+  port: string;
+  minMemGib?: string;
+  minStorageGib?: string;
+  minCpuThreads?: string;
+  minCpuCores?: string;
+  budget?: string;
+  link: string;
+};
 export class DepsaiApi {
   async getDepositAddress(user: string): Promise<`0x${string}`> {
     const response = await fetch(`${apiHost}/deposit?user=${user}`);
     return (await response.text()) as `0x${string}`;
+  }
+
+  async getDeployments(user: string): Promise<DeploymentDetails[]> {
+    const response = await fetch(`${apiHost}/deployments?user=${user}`);
+    return await response.json();
   }
 
   // dev: amount in token units
--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,6 +1,31 @@
+import { DeploymentDetails, depsaiApi } from '@api';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { WagmiStore } from '@modules/wagmi';
+import { withStores } from '@store';
+import { WithStores } from '@types';
+import { observer } from 'mobx-react-lite';
 
-export const Table = () => {
+const stores = {
+  wagmi: WagmiStore
+};
+
+const shortifyHash = (hash: string) => {
+  return hash.slice(0, 6) + '...' + hash.slice(-6);
+};
+const TableView: WithStores<typeof stores> = ({ wagmi }) => {
+  const [deployments, setDeployments] = useState([] as DeploymentDetails[]);
+
+  useEffect(() => {
+    if (!wagmi.account.address) return;
+    depsaiApi
+      .getDeployments(wagmi.account.address)
+      .then(d => {
+        setDeployments(d);
+      })
+      .catch(console.log);
+  });
+
   return (
     <div className="form-content">
       <button className="form-btn btn btn-primary">
@@ -16,20 +41,24 @@ export const Table = () => {
           </tr>
         </thead>
         <tbody className="table-body">
-          <tr className="table-tr">
-            <td className="table-td">Example</td>
-            <td className="table-td">Check</td>
-            <td className="table-td">
-              <a href="#" className="table-link">
-                link
-              </a>
-            </td>
-            <td className="table-td">
-              <button className="btn-manage">manage</button>
-            </td>
-          </tr>
+          {deployments.map((d, i) => (
+            <tr className="table-tr" key={i}>
+              <td className="table-td">{shortifyHash(d.package)}</td>
+              <td className="table-td">{'Active'}</td>
+              <td className="table-td">
+                <a href={d.link} className="table-link">
+                  link
+                </a>
+              </td>
+              <td className="table-td">
+                <button className="btn-manage">manage</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
   );
 };
+
+export const Table = withStores(stores)(observer(TableView));
